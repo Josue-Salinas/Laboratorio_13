b0c8d76e5b2cb6d65b5391cc9128284a3ff9d199
josue <josue@DESKTOP-LGFQ0HC>
2023-11-24 12:20:08 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_13/Calidad_Encuestas.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_13
+{
+    using System;
+
+    class EncuestasCalidad
+    {
+        private const int MaxRespuestas = 100;
+        private string[] respuestasTexto = new string[MaxRespuestas];
+        private int[] respuestasContador = new int[5]; 
+        private int totalRespuestas = 0;
+
+        public void RealizarEncuesta()
+        {
+            Console.Clear();
+            MostrarEncabezado("Nivel de Satisfacción");
+            Console.WriteLine("¿Qué tan satisfecho está con la atención de nuestra tienda?");
+            Console.WriteLine("1: Nada satisfecho\n2: No muy satisfecho\n3: Tolerable\n4: Satisfecho\n5: Muy satisfecho");
+            int opcion = int.Parse(Console.ReadLine());
+
+            if (opcion >= 1 && opcion <= 5)
+            {
+                respuestasContador[opcion - 1]++;
+                totalRespuestas++;
+                respuestasTexto[totalRespuestas - 1] = OpcionATexto(opcion);
+
+                Console.Clear();
+                MostrarEncabezado("¡Gracias por participar!");
+                MostrarMensaje("Presione una tecla para regresar al menú ...");
+                Console.ReadKey();
+            }
+            else
+            {
+                MostrarMensaje("Opción no válida. Inténtelo de nuevo.");
+                MostrarMensaje("Presione una tecla para regresar al menú ...");
+                Console.ReadKey();
+            }
+        }
+
+        public void VerDatosRegistrados()
+        {
+            Console.Clear();
+            MostrarEncabezado("Ver datos registrados");
+            MostrarDatos();
+
+            Console.WriteLine("\nPresione una tecla para regresar ...");
+            Console.ReadKey();
+        }
+
+        public void EliminarDato()
+        {
+            Console.Clear();
+            MostrarEncabezado("Eliminar un dato");
+            MostrarDatos();
+            Console.Write("Ingrese la posición a eliminar: ");
+            int posicion = int.Parse(Console.ReadLine());
+
+            if (EsPosicionValida(posicion))
+            {
+                int opcion = TextoAOpcion(respuestasTexto[posicion]);
+                respuestasContador[opcion - 1]--;
+                totalRespuestas--;
+
+                for (int i = posicion; i < totalRespuestas; i++)
+                {
+                    respuestasTexto[i] = respuestasTexto[i + 1];
+                }
+
+                Console.Clear();
+                MostrarEncabezado($"Posición {posicion} eliminada.");
+                MostrarMensaje("Presione una tecla para regresar ...");
+                Console.ReadKey();
+            }
+            else
+            {
+                MostrarMensaje("Posición no válida. Inténtelo de nuevo.");
+                MostrarMensaje("Presione una tecla para regresar ...");
+                Console.ReadKey();
+            }
+        }
+
+        public void OrdenarDatos()
+        {
+            Console.Clear();
+            MostrarEncabezado("Ordenar datos");
+            Array.Sort(respuestasTexto, 0, totalRespuestas);
+            MostrarDatos();
+
+            Console.WriteLine("\nPresione una tecla para regresar ...");
+            Console.ReadKey();
+        }
+
+        private static string OpcionATexto(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1: return "Nada satisfecho";
+                case 2: return "No muy satisfecho";
+                case 3: return "Tolerable";
+                case 4: return "Satisfecho";
+                case 5: return "Muy satisfecho";
+                default: return "";
+            }
+        }
+
+        private static int TextoAOpcion(string texto)
+        {
+            switch (texto)
+            {
+                case "Nada satisfecho": return 1;
+                case "No muy satisfecho": return 2;
+                case "Tolerable": return 3;
+                case "Satisfecho": return 4;
+                case "Muy satisfecho": return 5;
+                default: return -1;
+            }
+        }
+
+        private bool EsPosicionValida(int posicion)
+        {
+            return posicion >= 0 && posicion < totalRespuestas;
+        }
+
+        private void MostrarDatos()
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("Datos registrados");
+            Console.WriteLine("===============================");
+
+            for (int i = 0; i < totalRespuestas; i++)
+            {
+                Console.Write($"[{respuestasTexto[i]}] ");
+                if ((i + 1) % 5 == 0)
+                    Console.WriteLine();
+            }
+
+            MostrarResumen();
+        }
+
+        private void MostrarResumen()
+        {
+            Console.WriteLine("\nResumen:");
+            for (int i = 0; i < respuestasContador.Length; i++)
+            {
+                Console.WriteLine($"{respuestasContador[i]:D2} personas: {respuestasTexto[i]}");
+            }
+        }
+
+        private static void MostrarEncabezado(string titulo)
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine(titulo);
+            Console.WriteLine("===============================");
+        }
+
+        private static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+    }
+}
---
+++ Laboratorio_13/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_13
+{
+    class Program
+    {
+        static void Main()
+        {
+            EncuestasCalidad encuestas = new EncuestasCalidad();
+            int opcion;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("===============================");
+                Console.WriteLine("Encuestas de Calidad");
+                Console.WriteLine("===============================");
+                Console.WriteLine("1: Realizar Encuesta\n2: Ver datos registrados\n3: Eliminar un dato\n4: Ordenar datos de menor a mayor\n5: Salir");
+                Console.WriteLine("===============================");
+                Console.Write("Ingrese una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        encuestas.RealizarEncuesta();
+                        break;
+                    case 2:
+                        encuestas.VerDatosRegistrados();
+                        break;
+                    case 3:
+                        encuestas.EliminarDato();
+                        break;
+                    case 4:
+                        encuestas.OrdenarDatos();
+                        break;
+                    case 5:
+                        Console.WriteLine("¡Hasta luego!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Inténtelo de nuevo.");
+                        Console.WriteLine("Presione una tecla para regresar al menú ...");
+                        Console.ReadKey();
+                        break;
+                }
+
+            } while (opcion != 5);
+        }
+    }
+}
+
+
+
